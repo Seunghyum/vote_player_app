087510b1002aa30ecc53a43f229a53593a50dee0
Seunghyum <antmgu@gmail.com>
2023-12-30 14:01:58 +0900

    feat: add candidate detail page

---
+++ lib/features/candidates/candidate_detail_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:vote_player_app/constants/sizes.dart';
+
+class CandidateDetailScreen extends StatelessWidget {
+  final int id;
+  final String? imageUrl;
+  const CandidateDetailScreen({
+    super.key,
+    required this.id,
+    this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+          color: Colors.white,
+          child: SizedBox(
+            width: 100,
+            height: 100,
+            child: Hero(
+              tag: id,
+              child: CircleAvatar(
+                backgroundColor: Colors.grey.shade400,
+                foregroundImage:
+                    NetworkImage(imageUrl ?? 'https://picsum.photos/200/300'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/candidates/candidates_screen.dart
+++ lib/features/candidates/candidates_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vote_player_app/constants/sizes.dart';
+import 'package:vote_player_app/features/candidates/candidate_detail_screen.dart';
 import 'package:vote_player_app/models/candidate_model.dart';
 import 'package:vote_player_app/services/candidates_service.dart';
 
@@ -12,6 +13,33 @@ class CandidatesScreen extends StatefulWidget {
 
 class _CandidatesScreenState extends State<CandidatesScreen> {
   late Future<List<CandidateModel>> _candidates;
+
+  void _onListTileTap({required int id, String? imageUrl}) {
+    Navigator.of(context).push(
+      PageRouteBuilder(
+        opaque: false,
+        transitionDuration: const Duration(milliseconds: 200),
+        reverseTransitionDuration: const Duration(milliseconds: 200),
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return CandidateDetailScreen(
+            id: id,
+            imageUrl: imageUrl,
+          );
+        },
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(1.0, 0.0);
+          const end = Offset.zero;
+          const curve = Curves.ease;
+          final tween =
+              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+          return SlideTransition(
+            position: animation.drive(tween),
+            child: child,
+          );
+        },
+      ),
+    );
+  }
 
   @override
   void initState() {
@@ -44,8 +72,18 @@ class _CandidatesScreenState extends State<CandidatesScreen> {
                       padding:
                           const EdgeInsets.symmetric(horizontal: Sizes.size10),
                       child: ListTile(
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.grey.shade400,
+                        onTap: () => _onListTileTap(
+                          id: candidate.id,
+                          // imageUrl: null,
+                        ),
+                        leading: Hero(
+                          tag: candidate.id,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.grey.shade400,
+                            foregroundImage: const NetworkImage(
+                              'https://picsum.photos/200/300',
+                            ),
+                          ),
                         ),
                         title: Text(
                           candidate.name,
