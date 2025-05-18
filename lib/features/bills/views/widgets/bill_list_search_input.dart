f8571105c47192ea93f6e88ec91da49893c70d60
Seunghyum <antmgu@gmail.com>
2025-05-18 21:26:31 +0900

    feat: bill_list_search_input에 debounce 적용

--- lib/features/bills/views/widgets/bill_list_search_input.dart
+++ lib/features/bills/views/widgets/bill_list_search_input.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vote_player_app/features/bills/bloc/bill_list_bloc.dart';
+import 'package:easy_debounce/easy_debounce.dart';
 
-class BillListSearchInput extends StatelessWidget {
+class BillListSearchInput extends StatefulWidget {
   const BillListSearchInput({super.key});
 
+  @override
+  State<BillListSearchInput> createState() => _BillListSearchInputState();
+}
+
+class _BillListSearchInputState extends State<BillListSearchInput> {
   @override
   Widget build(BuildContext context) {
     return TextField(
       onChanged: (text) {
-        context.read<BillListBloc>().add(
-              SearchBillListEvent(
-                text,
-                context.read<BillListBloc>().selectedStatus,
-              ),
-            );
+        EasyDebounce.debounce(
+          'billListSearch', // 고유 키
+          const Duration(milliseconds: 400),
+          () {
+            context.read<BillListBloc>().add(
+                  SearchBillListEvent(
+                    text,
+                    context.read<BillListBloc>().selectedStatus,
+                  ),
+                );
+          },
+        );
       },
       decoration: InputDecoration(
         prefixIcon: const Icon(Icons.search),
